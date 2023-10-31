9ca32d6700dd523ffefabd7d5146976023b29f69
Aiden Ow <aidenow.22@ichat.sp.edu.sg>
2023-10-31 15:21:53 +0800

    Modified

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,1 +1,22 @@
-import Lab-2.bmi as bmi
+import Lab2.bmi as bmi
+
+def test_bmi_normal_weight():
+    height = 1.73
+    weight = 57
+    result = bmi.calculate_bmi(height, weight)
+
+    assert (result == 0)
+
+def test_bmi_over_weight():
+    height = 1.73
+    weight = 100
+    result = bmi.calculate_bmi(height, weight)
+
+    assert (result == 1)
+
+def test_bmi_under_weight():
+    height = 1.73
+    weight = 20
+    result = bmi.calculate_bmi(height, weight)
+
+    assert (result == -1)
